9cb10e3661e2feb808c04c101e8d0f9df1f029e1
CISstudent <cisstudent@CISstudents-MacBook-Pro-7.local>
2016-11-18 12:23:50 -0700

    Init

--- Calorie Tracker/ViewController.swift
+++ Calorie Tracker/ViewController.swift
@@ -9,8 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
+        //hello
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
