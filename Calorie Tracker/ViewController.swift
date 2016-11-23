e36dca7834170887ff383badc5307429876c3316
cisstudent <cisstudent@cisstudents-MacBook-Pro-4.local>
2016-11-23 13:40:23 -0700

    Began building the initial UI

--- Calorie Tracker/ViewController.swift
+++ Calorie Tracker/ViewController.swift
@@ -9,20 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
 
     override func viewDidLoad() {
-        //hello
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
